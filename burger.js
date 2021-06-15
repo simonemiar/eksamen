0fcf370b1033ccae9f1b10410b78e50cced11974
simonemiar <79079392+simonemiar@users.noreply.github.com>
2021-06-15 19:13:19 +0200

    kopi

--- burger.js
+++ burger.js
@@ -1,21 +1,23 @@
 "use strict";
 
+const burgerButton = document.querySelector(".burgerButton");
+const streg1 = document.querySelector(".streg1");
+const streg2 = document.querySelector(".streg2");
+const streg3 = document.querySelector(".streg3");
+const navbarLinks = document.querySelector(".navbarLinks");
+
 // Når HTML og CSS er loaded startes sidenVises
 window.addEventListener("load", sidenVises);
 
-// Når #burger_btn er "clicked" startes toggleMenu
+// Når #burger_btn er "clicked" startes openMenu
 function sidenVises() {
-    const burgerBtn = document.querySelector("#burger_btn");
-    burgerBtn.addEventListener("click", toggleMenu);
-
+  burgerButton.addEventListener("click", open);
 }
 
-// Hvis #burger_btn og #menu ikke har classen .open, addes den.
-// Hvis de omvendt har .open, removes den
-function toggleMenu() {
-    const burgerBtn = this;
-    const menu = document.querySelector("#menu");
-
-    burgerBtn.classList.toggle("open");
-    menu.classList.toggle("open");
+function open() {
+  console.log("open menu");
+  streg1.classList.toggle("aktiv");
+  streg2.classList.toggle("aktiv");
+  streg3.classList.toggle("aktiv");
+  navbarLinks.classList.toggle("aktiv");
 }
